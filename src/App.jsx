b4e1f493ccb207f0bcda4efcefa5c0601b25742a
mudassirkarar3mk20-spec <mudassirkarar.3.mk20@gmail.com>
2026-05-09 20:36:10 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+return (
+<div style={{
+minHeight:"100vh",
+background:"#060608",
+color:"#fff",
+display:"flex",
+alignItems:"center",
+justifyContent:"center",
+fontFamily:"Arial"
+}}>
+PropBot Live ✅
+
+);
+}
